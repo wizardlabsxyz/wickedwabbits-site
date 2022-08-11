08fe8a2134560939deb71d381f78666d1be8cf46
wizard <wizardlabsxyz@gmail.com>
2022-08-10 19:11:04 -0700

    actually fix whitelist file import

--- src/components/WhitelistChecker.js
+++ src/components/WhitelistChecker.js
@@ -4,10 +4,11 @@ function WhitelistChecker() {
 
     return (
         <button onClick={async () => {
-            const response = await fetch('/.netlify/functions/checkWhitelist')
-            .then(
-                response => response.json()
-            )
+
+            const response = await fetch('/.netlify/functions/checkWhitelist', {
+                method: 'POST',
+                body: JSON.stringify({ address: '0x0AcFE2ED7e0da4e36dd582840Db65bE341FacC2a' })
+            }).then(response => response.json());
 
             console.log(JSON.stringify(response));
         }}>Click me</button>
